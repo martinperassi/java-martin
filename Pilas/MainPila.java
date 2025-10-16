02f054bd4e8408351fb466e4f7ddd7c3af791876
martinperassi43 <martinperassi4333@gmail.com>
2025-10-16 17:40:22 -0300

    entendiendo como funcionan las pilas utilizando un vector

---
+++ Pilas/MainPila.java
@@ -0,0 +1,27 @@
+package Pilas;
+
+public class MainPila {
+    public static void main(String[] args) {
+        Pila pila = new Pila();
+
+        System.out.println(pila.estaVacia());//devuelve true si la pila esta vacia
+        pila.meter(10);
+        pila.meter(4);
+        pila.meter(5);
+        pila.meter(6);
+        pila.meter(17);
+
+        int elemento;
+
+        if(!pila.estaVacia()){
+            elemento = pila.sacar();
+            System.out.println("elemento sacado: " + elemento);
+        }else{
+            System.out.println("error, la pila esta vacia, por lo tanto no se puede sacar un elemento.");
+        }
+        System.out.println(pila.estaVacia());// --> esta vacia (dara true)
+        System.out.println(pila.estaLlena());// --> no esta llena (dara false)
+
+
+    }
+}
---
+++ Pilas/Punto1.java
@@ -0,0 +1,5 @@
+package Pilas;
+
+public class Punto1 {
+    
+}
