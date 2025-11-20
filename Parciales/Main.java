7990505cb58771954e4c191f3e655cc79335e7d9
martin perassi <martinperassi4333@gmail.com>
2025-11-20 00:10:59 -0300

    ejercicio terminado parcial pilas y colas

--- Parciales/Main.java
+++ Parciales/Main.java
@@ -21,29 +21,52 @@ public class Main {
         pila2.meter(14);
 
         System.out.println(pila.compararPares(pila2));
+        System.out.println();
 
+        //creamos las pilas de caracteres y sus auxiliares para poder devolverlas a su estado original.
         PilaCaracteres p1 = new PilaCaracteres();
         PilaCaracteres p2 = new PilaCaracteres();
+        PilaCaracteres p1Aux = new PilaCaracteres();
+        PilaCaracteres p2Aux = new PilaCaracteres();
 
         System.out.println(p1.meter('a'));
         System.out.println(p1.meter('s'));
         System.out.println(p1.meter('f'));
-
-        System.out.println(p1.meter('e'));
-        System.out.println(p1.meter('i'));
-        System.out.println(p1.meter('o'));
         System.out.println();
 
+        System.out.println(p2.meter('e'));
+        System.out.println(p2.meter('i'));
+        System.out.println(p2.meter('o'));
+        System.out.println();
+
+        //creamos la cola de caracteres 
         Cola cola = new Cola();
-        while(!cola.estaLlena()){
-            char aux1,aux2;
-                aux1 = p1.sacar();
-                aux2 = p2.sacar();
-                cola.meter(aux1);
-                cola.meter(aux2);
-                System.out.println(aux1);
-                System.out.println(aux2);
+        while(!p1.estaVacia() && !p2.estaVacia()){
+            char aux1 = p1.sacar();
+            char aux2 = p2.sacar();
+            cola.meter(aux1);
+            cola.meter(aux2);
+            p1Aux.meter(aux1);
+            p2Aux.meter(aux2);
         }
+
+        //restauramos pila1
+        while(!p1Aux.estaVacia()){
+            char aux = p1Aux.sacar();
+            p1.meter(aux);
+        }
+        //restauramos pila2
+        while(!p2Aux.estaVacia()){
+            char aux = p2Aux.sacar();
+            p2.meter(aux);
+        }
+
+        System.out.println("Contenido de la cola: ");
+        while(!cola.estaVacia()){
+            char aux = cola.sacar();
+            System.out.println(aux);
+        }
+
 
 
 
